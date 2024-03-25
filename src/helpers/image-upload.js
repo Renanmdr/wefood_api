885b90f966ffcc1e8c9435bdb81ecc657fb7ab68
Renanmdr <renanmdr3@gmail.com>
2024-03-25 02:03:25 -0300

    adding src folder

--- src/helpers/image-upload.js
+++ src/helpers/image-upload.js
@@ -12,7 +12,7 @@ const imageStorage = multer.diskStorage({
             folder = 'foods'
         }
 
-        cb(null, `public/images/${folder}`)
+        cb(null, `./src/public/images/${folder}`)
     },
     filename: function (req, file, cb){
         cb(null, Date.now() + String(Math.floor(Math.random() * 1000)) + path.extname(file.originalname))
